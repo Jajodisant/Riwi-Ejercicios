c445eaecacafb8707701527167cdcb569ea959c7
Alexis-pr <53275027+Alexis-pr@users.noreply.github.com>
2025-11-11 21:28:48 -0500

    Add files via upload

---
+++ Python/Exercise/Day3/pythonbank.py
@@ -0,0 +1,10 @@
+""" 4. Banco “PythonBank” – Evaluador de crédito
+Como asesor financiero, quiero una función evaluar_credito(ingresos, edad) que:
+
+    Apruebe el crédito si los ingresos son mayores de 2 millones y la edad está entre 25 y 60.
+    Si no cumple, mostrar “Crédito rechazado”.
+    Usar condicionales dentro de la función.
+ """
+
+
+a
